0b992190f12c9e711fbd72bd471ef4ccace1ecd1
Mateusz Janasiak <mjanasiak@9bits.pl>
2023-01-17 15:50:45 +0100

    Comitt

--- FirstCourseApp/CsvReader/CsvReader.cs
+++ FirstCourseApp/CsvReader/CsvReader.cs
@@ -6,11 +6,11 @@ namespace FirstCourseApp.CsvReader;
     public class CsvReader : ICsvReader
      
     {
-        public List<ClassModel> ProcessClient(string filePath)
+        public List<ClientCsv> ProcessClient(string filePath)
         {
             if (!File.Exists(filePath))
             {
-            return new List<ClassModel>();
+            return new List<ClientCsv>();
             }     
         
             var clients = 
@@ -20,6 +20,22 @@ namespace FirstCourseApp.CsvReader;
 
             return clients.ToList();
         }
+
+    public List<EmployeeCsv> ProcessEmployee(string filePath)
+    {
+        if (!File.Exists(filePath))
+        {
+            return new List<EmployeeCsv>();
+        }
+
+        var employees =
+            File.ReadAllLines(filePath)
+            .Skip(1)
+            .Where(x => x.Length > 1)
+            .ToEmployee();
+
+        return employees.ToList();
     }
+}
 
 
