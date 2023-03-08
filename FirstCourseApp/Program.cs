0e7762a6973e497d732aa9894c261886ac3cc590
Mateusz Janasiak <mjanasiak@9bits.pl>
2023-03-08 10:40:18 +0100

    ConfigurationFile
    add configuration file appsettings.json

--- FirstCourseApp/Program.cs
+++ FirstCourseApp/Program.cs
@@ -5,21 +5,28 @@ using FirstCourseApp.Entities;
 using FirstCourseApp.Repositories;
 using FirstCourseApp.Repositories.Interface;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using RabbitMQ.Client;
+using System.Configuration;
+
+var configuration = new ConfigurationBuilder()
+   .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+   .Build();
 
 var services = new ServiceCollection();
-services.AddSingleton<IApp,App>();
-services.AddSingleton<IRepository<Client>,ListRepository<Client>>();
-services.AddSingleton<ICsvReader,CsvReader>();
+services.AddSingleton<IApp, App>();
+services.AddSingleton<IRepository<Client>, ListRepository<Client>>();
+services.AddSingleton<ICsvReader, CsvReader>();
 
 services.AddDbContext<FirstCourseAppDbContext>(options => options
-    .UseSqlServer("Data Source =.\\SQLEXPRESS; Initial Catalog = TEST2; Integrated Security = True"));
-
+          .UseSqlServer(configuration.GetConnectionString("Baza")));
+                            
 var serviceProvider = services.BuildServiceProvider();
+        
 var app = serviceProvider.GetService<IApp>();
 
 app.Run();
 
 
-
+ 
